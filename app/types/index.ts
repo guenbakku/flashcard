7cec20971beb1b363e2371f13c5ea1c57ad1df98
nvbach <vanbach1213@gmail.com>
2026-05-19 22:04:02 +0700

    feat: introduce data-transfer (#4)

--- app/types/index.ts
+++ app/types/index.ts
@@ -13,18 +13,25 @@ export const deckMetaSchema = z.object({
   cardCount: z.number(),
 });
 
+export const deckProgressSchema = z.object({
+  lastStudied: z.iso.datetime().nullable(),
+  masteredCards: z.record(z.string(), z.boolean()),
+});
+
+export const deckProgressStorageSchema = z.record(
+  z.string(),
+  deckProgressSchema,
+);
+
 export const deckDetailSchema = z.object({
   cards: z.array(cardSchema),
 });
 
 export type Card = z.infer<typeof cardSchema>;
+
 export type DeckMeta = z.infer<typeof deckMetaSchema>;
 export type DeckDetail = z.infer<typeof deckDetailSchema>;
-
-export type DeckProgress = {
-  lastStudied: string | null;
-  masteredCards: Record<string, true>;
-};
+export type DeckProgress = z.infer<typeof deckProgressSchema>;
 
 export type Deck = DeckMeta & DeckProgress & {
   progress: number;
