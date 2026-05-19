8de5e84e554327ab5561dad9702211a8da5dfed4
Nguyen Van Bach <vanbach1213@gmail.com>
2026-05-19 12:04:08 +0700

    feat: error handling when fetch api

--- app/composables/use-client-fetch.ts
+++ app/composables/use-client-fetch.ts
@@ -4,10 +4,18 @@ import type { UseFetchOptions } from '#app';
  * Fetch data from API only on client-side
  */
 const useClientFetch = <T>(url: string | (() => string), options: UseFetchOptions<T> = {}) => {
-  return useFetch(url, {
+  const result = useFetch(url, {
     ...options,
     server: false, // Turn off server-side fetching
   });
+
+  watch(result.error, (err) => {
+    if (err) {
+      console.error(`[useClientFetch] ${url}:`, err);
+    }
+  });
+
+  return result;
 };
 
 export default useClientFetch;
