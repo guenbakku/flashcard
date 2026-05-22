60a7f7463cf6589c7e73d39db03cc23b2a19e5ee
nvbach <vanbach1213@gmail.com>
2026-05-22 20:14:59 +0700

    fix: force browser cache refresh for JSON files after nuxt generate (#18)
    Ensures users always receive the latest version of JSON data immediately following a new deployment.

--- app/composables/use-client-fetch.ts
+++ app/composables/use-client-fetch.ts
@@ -4,14 +4,23 @@ import type { UseFetchOptions } from '#app';
  * Fetch data from API only on client-side
  */
 const useClientFetch = <T>(url: string | (() => string), options: UseFetchOptions<T> = {}) => {
-  const result = useFetch(url, {
+  let urlValue = toValue(url);
+
+  // Add buildVersion as query parameter to JSON files to prevent caching issues of browser.
+  // This ensure user always get the latest version of JSON files after each build.
+  if (urlValue.endsWith('.json')) {
+    const { public: { buildVersion } } = useRuntimeConfig();
+    urlValue = `${urlValue}?${buildVersion}`;
+  }
+
+  const result = useFetch(urlValue, {
     ...options,
     server: false, // Turn off server-side fetching
   });
 
   watch(result.error, (err) => {
     if (err) {
-      console.error(`[useClientFetch] ${url}:`, err);
+      console.error(`[useClientFetch] ${urlValue}:`, err);
     }
   });
 
