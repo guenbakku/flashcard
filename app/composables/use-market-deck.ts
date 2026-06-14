92c5aee009a0a0e5e70b1829a918f4976fe69d4a
Nguyen Van Bach <vanbach1213@gmail.com>
2026-06-14 16:35:51 +0700

    fix: improve error handling in market decks

--- app/composables/use-market-deck.ts
+++ app/composables/use-market-deck.ts
@@ -1,12 +1,13 @@
 import { type DeckDetail, deckDetailSchema } from '~/types';
 
 const useMarketDeck = (id: string) => {
-  const { data, pending } = useClientFetch<DeckDetail>(`/data/decks/${id}.json`, {
+  const { data, pending, error } = useClientFetch<DeckDetail>(`/data/decks/${id}.json`, {
     transform: raw => deckDetailSchema.parse(raw),
   });
 
   return {
     data,
+    error,
     pending,
   };
 };
