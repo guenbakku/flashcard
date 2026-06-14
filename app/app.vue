324cd884dd0b80cb1d3511a271d83a5ff2a74495
Nguyen Van Bach <vanbach1213@gmail.com>
2026-06-14 16:27:22 +0700

    fix: revert NuxtPwaManifest

--- app/app.vue
+++ app/app.vue
@@ -36,8 +36,8 @@ useSeoMeta({
 <template>
   <UApp>
     <NuxtLoadingIndicator />
+    <NuxtPwaManifest />
     <NuxtLayout>
-      <PwaUpdatePrompt />
       <NuxtPage />
     </NuxtLayout>
   </UApp>
